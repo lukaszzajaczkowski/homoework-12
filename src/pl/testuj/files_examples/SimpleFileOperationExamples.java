9861fb757c8ad43ae8774c3b04ca69bf6684480b
lukaszzajaczkowski <clancys49@gmail.com>
2021-10-09 17:56:03 +0200

    homowork-12 part2

--- src/pl/testuj/files_examples/SimpleFileOperationExamples.java
+++ src/pl/testuj/files_examples/SimpleFileOperationExamples.java
@@ -6,63 +6,34 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import static java.io.IOException.*;
+
 /*
 Zadanie domowe: napisać metodę writeToFileWithoutTry, która robi to samo co writeToFile
 tylko bez bloku try...catch
  */
 public class SimpleFileOperationExamples {
-    public static void main(String[] args) {
-        // System.getProperty() - pobierze scieżkę do projektu
-        //String pathToFile = System.getProperty("user.dir") + "\\names_base.txt"; // wersja dla Windows
-        String pathToFile = System.getProperty("user.dir") + "/names_base.txt"; // wersja dla macOS (macbook)
+    public static void main(String[] args) throws IOException {
 
-        writeToFile(pathToFile, "Zenek");
-        writeToFile(pathToFile, "Julia");
+        String pathToFile = System.getProperty("user.dir") + "\\homework.txt";
 
-        readFromFile(pathToFile);
+        writeToFileWithoutTry(pathToFile, null);
+
     }
 
-    public static void writeToFile(String filePath, String textToSave) {
-        // obiekt do zapisu danych do pliku
+
+    public static void writeToFileWithoutTry(String filePath, String textToSave) throws IOException {
         FileWriter fileWriter = null;
-        try {
+        if (textToSave != null){
             fileWriter = new FileWriter(filePath, true);
-            fileWriter.write(textToSave + "\n");
-        } catch (IOException e) {
-            System.out.println("Błąd zapisu do pliku");
-            e.printStackTrace();
-        } finally {
-            // powtórzenie try...catch ponieważ ciągle działamy na połączeniu do pliku
-            try {
+            fileWriter.write("zapisany tekst to: " + "\n" + textToSave + "\n");
+        } else {
+            throw new IOException("Błędny tekst: " + textToSave);
+        }
                 fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        }
-    }
-
-    public static void readFromFile(String filePath) {
-        File file = new File(filePath);
-        Scanner scanner = null;
-
-        try {
-            scanner = new Scanner(file);
-            // Należy upewnić się czy istnieje kolejna linia
-            // przed próbą jej popbrania - jeśli pobierzemy linię, która nie istnieje
-            // to będzie rzucony wyjątek
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine(); // pobranie zawartości linii
-                System.out.println(line);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
-        }
-    }
 }
+
 
 
 
